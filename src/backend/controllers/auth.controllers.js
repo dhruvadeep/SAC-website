abdca075da756cb049f28d55a588344b98d1b1c8
smiling007 <lokeshsaimuvva@gmail.com>
2024-03-26 00:48:44 +0530

    Implemented Auth Functions

--- src/backend/controllers/auth.controllers.js
+++ src/backend/controllers/auth.controllers.js
@@ -6,4 +6,35 @@ function AuthUser(req, accessToken, refreshToken, profile, done) {
     return done(null, profile);
 }
 
-module.exports = { AuthUser };
+function isLoggedIn(req, res, next){
+    if (req.user == null) {
+        res.status(403);
+        return res.send('You need to sign in');
+    }
+    next();
+}
+
+const authRole = (roles) => {
+    return (req, res, next) => {
+        if (roles.includes(req.user.role)) {
+            next();
+        }
+        else {
+            res.sendstatus(401);
+            return res.send('Not Allowed');
+        }
+    }
+}
+
+const canViewUser = (req, res, next) => {
+    const userId = parseInt(req.params.id)
+    if (req.user.id === userId) {
+        next();
+    }
+    else {
+        res.sendstatus(401);
+        return res.send('Not Allowed');
+    }
+} 
+
+module.exports = { AuthUser, isLoggedIn, authRole, canViewUser };
